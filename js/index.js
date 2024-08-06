1b9e0227374a78a647017438ea8010ac1aa16f20
John Chin-Jew <johnchinjew@gmail.com>
2024-08-06 09:40:03 -0700

    reset score upon collision

--- js/index.js
+++ js/index.js
@@ -79,6 +79,7 @@ function update() {
   launchShurikens();
   moveShurikens();
   scoreShurikens();
+  resetScore();
   deleteShurikens();
 }
 
@@ -171,7 +172,21 @@ function scoreShurikens() {
 }
 
 function resetScore() {
-  // TODO: detect collision and then reset score
+  for (const shuriken of shurikens) {
+    const collisionDetected = isColliding(player, shuriken);
+    if (collisionDetected) {
+      score = 0;
+    }
+  }
+}
+
+function isColliding(rect1, rect2) {
+  return (
+    rect1.x < rect2.x + rect2.width &&
+    rect1.x + rect1.width > rect2.x &&
+    rect1.y < rect2.y + rect2.height &&
+    rect1.y + rect1.height > rect2.y
+  );
 }
 
 // Render to canvas
